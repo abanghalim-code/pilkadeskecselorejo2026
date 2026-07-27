ea9f7a76f509411a1d64f0e0c145578d3d92104b
abanghalim-code <sos.halim@gmail.com>
2026-07-27 09:41:42 +0700

    Update agenda.js

--- assets/js/agenda.js
+++ assets/js/agenda.js
@@ -1,1 +1,17 @@
-
+const tahapan = [
+{
+    nama: "Penyusunan RAB Pilkades",
+    mulai: "2026-07-27",
+    selesai: "2026-09-08"
+},
+{
+    nama: "Pemutakhiran Data Pemilih",
+    mulai: "2026-07-29",
+    selesai: "2026-07-31"
+},
+{
+    nama: "Penetapan DPS",
+    mulai: "2026-08-03",
+    selesai: "2026-08-03"
+}
+];
