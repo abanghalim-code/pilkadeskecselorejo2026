bbea246f2c2bf4549b001e345c837a1983eec8c8
abanghalim-code <sos.halim@gmail.com>
2026-09-06 21:35:22 +0700

    make bacalon.js

---
+++ assets/js/bacalon.js
@@ -0,0 +1,262 @@
+// Master Data Bakal Calon Kepala Desa
+// Pilkades Serentak Kecamatan Selorejo 2026
+// Sumber: bacalon(4).xlsx
+
+const bacalon = [
+  {
+    "id": "BJR-001",
+    "desa": "Banjarsari",
+    "no_urut": 1,
+    "nama": "BAMBANG SUTEDJO",
+    "tempat_lahir": "SURABAYA",
+    "tanggal_lahir": "17 April 1964",
+    "tanggal_daftar": "02 September 2026",
+    "alamat": "Kalilegi, Banjarsari",
+    "hape": "+62 823-3376-0699",
+    "pendidikan": "S 1 (1992)",
+    "jurusan": "Kependidikan",
+    "sekolah": "SMK Muhammadiyah Kepanjen",
+    "pekerjaan": "KEPALA DESA",
+    "riwayat_pendidikan": [
+      "SD",
+      "SMP",
+      "SMA",
+      "S1"
+    ],
+    "pengalaman": [
+      "Petani",
+      "Kepala Desa 2019-2024-2026"
+    ],
+    "visi": "“Mewujudkan Desa yang maju, mandiri, sejahtera, dan berdaya saing melalui pemerintahan yang transparan serta pembangunan yang berkelanjutan.”",
+    "misi": [
+      "Meningkatkan kualitas pelayanan pemerintahan desa yang cepat, mudah, dan transparan;",
+      "Mewujudkan pembangunan infrastruktur yang merata dan berkelanjutan;",
+      "Meningkatkan kesejahteraan masyarakat melalui pemberdayaan ekonomi lokal;",
+      "Memperkuat gotong royong dan partisipasi masyarakat dalam pembangunan desa."
+    ],
+    "motto": "“Bersama Membangun, Bersama Sejahtera.”",
+    "foto_resmi": "",
+    "foto_informal": ""
+  },
+  {
+    "id": "BJR-002",
+    "desa": "Banjarsari",
+    "no_urut": 2,
+    "nama": "SUWARDI",
+    "tempat_lahir": "BLITAR",
+    "tanggal_lahir": "25 Desember 1971",
+    "tanggal_daftar": "02 September 2026",
+    "alamat": "Kalilegi, Banjarsari",
+    "hape": "+62 823-3376-0000",
+    "pendidikan": "SMA",
+    "jurusan": "Kependidikan",
+    "sekolah": "SMK Muhammadiyah Kepanjen",
+    "pekerjaan": "KEPALA DESA",
+    "riwayat_pendidikan": [
+      "SD",
+      "SMP",
+      "SMA"
+    ],
+    "pengalaman": [
+      "Kepala Desa 2013-2018;",
+      "Petani"
+    ],
+    "visi": "“Mewujudkan pemerintahan desa yang bersih, terbuka, responsif, dan memberikan pelayanan terbaik bagi seluruh masyarakat.”",
+    "misi": [
+      "Mewujudkan pemerintahan desa yang bersih, jujur, dan akuntabel;",
+      "Meningkatkan pelayanan kesehatan, pendidikan, dan kesejahteraan sosial masyarakat;",
+      "Mengembangkan potensi pertanian, UMKM, dan usaha masyarakat;",
+      "Meningkatkan keterbukaan informasi serta partisipasi masyarakat dalam pengambilan keputusan."
+    ],
+    "motto": "“Melayani dengan Hati, Membangun dengan Bukti.”",
+    "foto_resmi": "",
+    "foto_informal": ""
+  },
+  {
+    "id": "BJR-003",
+    "desa": "Banjarsari",
+    "no_urut": 3,
+    "nama": "SUNARDI",
+    "tempat_lahir": "BLITAR",
+    "tanggal_lahir": "25 Desember 1971",
+    "tanggal_daftar": "02 September 2026",
+    "alamat": "Kalilegi, Banjarsari",
+    "hape": "+62 823-3376-0000",
+    "pendidikan": "SMA",
+    "jurusan": "Kependidikan",
+    "sekolah": "SMK Muhammadiyah Kepanjen",
+    "pekerjaan": "PENSIUNAN POLRI",
+    "riwayat_pendidikan": [
+      "SD",
+      "SMP",
+      "SMA"
+    ],
+    "pengalaman": [
+      "Kapolsek Selorejo;",
+      "Pensiun."
+    ],
+    "visi": "“Mewujudkan Desa yang sejahtera melalui penguatan ekonomi masyarakat, pemberdayaan UMKM, pertanian, dan pengembangan potensi desa.”",
+    "misi": [
+      "Mengembangkan potensi ekonomi desa untuk menciptakan lapangan kerja dan meningkatkan pendapatan masyarakat;",
+      "Mengoptimalkan pengelolaan BUMDes secara profesional dan transparan;",
+      "Meningkatkan kualitas sumber daya manusia melalui pelatihan dan pemberdayaan;",
+      "Mendorong inovasi dan pemanfaatan teknologi dalam pembangunan desa."
+    ],
+    "motto": "“Desa Maju, Masyarakat Sejahtera.”",
+    "foto_resmi": "",
+    "foto_informal": ""
+  },
+  {
+    "id": "SDM-001",
+    "desa": "Sidomulyo",
+    "no_urut": 1,
+    "nama": "WAKHID BASUKI",
+    "tempat_lahir": "BLITAR",
+    "tanggal_lahir": "25 Desember 1971",
+    "tanggal_daftar": "02 September 2026",
+    "alamat": "Sidomulyo, Sidomulyo",
+    "hape": "+62 823-3376-0000",
+    "pendidikan": "SMA",
+    "jurusan": "Kependidikan",
+    "sekolah": "SMK Muhammadiyah Kepanjen",
+    "pekerjaan": "KEPALA DESA",
+    "riwayat_pendidikan": [
+      "SD",
+      "SMP",
+      "SMA"
+    ],
+    "pengalaman": [
+      "Kepala Desa 2013-2018;",
+      "Petani"
+    ],
+    "visi": "“Membangun Desa yang guyub, rukun, aman, dan sejahtera dengan menguatkan semangat gotong royong dan partisipasi masyarakat.”",
+    "misi": [
+      "Meningkatkan pemerataan pembangunan hingga seluruh dusun dan wilayah desa;",
+      "Meningkatkan kualitas jalan, drainase, irigasi, dan fasilitas umum;",
+      "Menciptakan lingkungan desa yang aman, bersih, sehat, dan nyaman;",
+      "Memperkuat kerukunan, kebersamaan, dan kepedulian sosial masyarakat."
+    ],
+    "motto": "“Jujur dalam Memimpin, Nyata dalam Bertindak.”",
+    "foto_resmi": "",
+    "foto_informal": ""
+  },
+  {
+    "id": "SDM-002",
+    "desa": "Sidomulyo",
+    "no_urut": 2,
+    "nama": "SUMAJI",
+    "tempat_lahir": "BLITAR",
+    "tanggal_lahir": "15 Januari 1976",
+    "tanggal_daftar": "02 September 2026",
+    "alamat": "Gunungsari, Sidomulyo",
+    "hape": "+62 812-1773-0906",
+    "pendidikan": "SMA",
+    "jurusan": "Kependidikan",
+    "sekolah": "SMK Muhammadiyah Kepanjen",
+    "pekerjaan": "KEPALA DESA",
+    "riwayat_pendidikan": [
+      "SD",
+      "SMP",
+      "SMA"
+    ],
+    "pengalaman": [
+      "Petani",
+      "Kepala Desa 2019-2024-2026"
+    ],
+    "visi": "“Mewujudkan Desa yang inovatif dan adaptif melalui pemberdayaan pemuda, pemanfaatan teknologi, serta peningkatan kualitas sumber daya manusia.”",
+    "misi": [
+      "Meningkatkan peran pemuda dalam pembangunan dan kegiatan produktif desa;",
+      "Mengembangkan UMKM, ekonomi kreatif, serta potensi wisata desa;",
+      "Mendorong digitalisasi pelayanan dan administrasi pemerintahan desa;",
+      "Meningkatkan keterampilan masyarakat melalui pelatihan dan pendampingan usaha."
+    ],
+    "motto": "“Dari Rakyat, Untuk Rakyat, Bersama Rakyat.”",
+    "foto_resmi": "",
+    "foto_informal": ""
+  },
+  {
+    "id": "SBR-001",
+    "desa": "Sumberagung",
+    "no_urut": 1,
+    "nama": "EKO HARMONO",
+    "tempat_lahir": "BLITAR",
+    "tanggal_lahir": "18 januari 1977",
+    "tanggal_daftar": "02 September 2026",
+    "alamat": "Kepel, Sumberagung",
+    "hape": "+62 856-4952-6868",
+    "pendidikan": "SMA",
+    "jurusan": "Kependidikan",
+    "sekolah": "SMK Muhammadiyah Kepanjen",
+    "pekerjaan": "KEPALA DESA",
+    "riwayat_pendidikan": [
+      "SD",
+      "SMP",
+      "SMA"
+    ],
+    "pengalaman": [
+      "Kepala Desa 2013-2018;",
+      "Kepala Desa 2019-2024-2026;"
+    ],
+    "visi": "“Mewujudkan Desa yang nyaman dan sejahtera melalui pemerataan pembangunan infrastruktur, peningkatan pelayanan dasar, dan penguatan kesejahteraan masyarakat.”",
+    "misi": [
+      "Meningkatkan kesejahteraan petani, pelaku usaha kecil, dan kelompok masyarakat produktif;",
+      "Meningkatkan akses masyarakat terhadap pelayanan kesehatan dan pendidikan;",
+      "Mengembangkan program perlindungan dan pemberdayaan masyarakat kurang mampu;",
+      "Mengoptimalkan pengelolaan sumber daya desa untuk kepentingan masyarakat."
+    ],
+    "motto": "“Kerja Nyata untuk Desa yang Lebih Baik.”",
+    "foto_resmi": "",
+    "foto_informal": ""
+  },
+  {
+    "id": "SBR-002",
+    "desa": "Sumberagung",
+    "no_urut": 2,
+    "nama": "CASLAM",
+    "tempat_lahir": "BLITAR",
+    "tanggal_lahir": "25 Desember 1971",
+    "tanggal_daftar": "02 September 2026",
+    "alamat": "Sumberwader, Sumberagung",
+    "hape": "+62 823-3353-1944",
+    "pendidikan": "SMA",
+    "jurusan": "Kependidikan",
+    "sekolah": "SMK Muhammadiyah Kepanjen",
+    "pekerjaan": "PENSIUNAN TNI",
+    "riwayat_pendidikan": [
+      "SD",
+      "SMP",
+      "SMA"
+    ],
+    "pengalaman": [
+      "Pensiun TNI;",
+      "Petani"
+    ],
+    "visi": "“Mewujudkan Desa yang adil, inklusif, dan sejahtera dengan pembangunan yang merata serta melibatkan seluruh unsur masyarakat dalam setiap proses pembangunan.”",
+    "misi": [
+      "Mewujudkan pemerintahan desa yang dekat, responsif, dan berorientasi pada kebutuhan masyarakat;",
+      "Membangun desa secara adil dengan mengutamakan pemerataan dan kebermanfaatan;",
+      "Meningkatkan kualitas pelayanan publik serta keterbukaan pengelolaan anggaran desa;",
+      "Mewujudkan masyarakat yang mandiri, sejahtera, guyub, dan berdaya saing."
+    ],
+    "motto": "“Guyub, Transparan, dan Berkemajuan.”",
+    "foto_resmi": "",
+    "foto_informal": ""
+  }
+];
+
+// Ambil semua bacalon berdasarkan desa dan urutkan berdasarkan nomor urut.
+function getBacalonByDesa(desa) {
+  return bacalon
+    .filter(item => item.desa === desa)
+    .sort((a, b) => Number(a.no_urut) - Number(b.no_urut));
+}
+
+// Ambil satu bacalon berdasarkan ID permanen.
+function getBacalonById(id) {
+  return bacalon.find(item => item.id === id);
+}
+
+// Daftar desa yang tersedia.
+function getDesaBacalon() {
+  return [...new Set(bacalon.map(item => item.desa))];
+}
